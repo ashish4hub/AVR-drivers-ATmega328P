c1e21b9a7481765b40725bd30da762f1cdda14fc
ashish4hub <ashish.k620045@gmail.com>
2026-08-04 00:05:39 +0530

    Added msg_src and hscr04_src

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 #define F_CPU 16000000UL
 
 #include "CLI/CLI.h"
-#include <avr/interrupt.h>
 
 int main(void){
 
     USART_init(9600);                 // UART initialization at 9600 BUAD 
-    pwm_init(1000,pwm_TIMER1);       // Frequency = 1000 and TIMER1 PWM initialization
+    //pwm_init(1000,pwm_TIMER1);       // Frequency = 1000 and TIMER1 PWM initialization
     timer_init();                   // TIMER0 initialization for timing (counting)
-    
+    HCSR04_init();                 // HCSR04 initialization 
+
     /* ADC Configuration */
     ADC_config_t adc_config = {
         .reference = ADC_REF_VCC,
@@ -20,17 +20,17 @@ int main(void){
 
     ADC_init(&adc_config);      // ADC initialization
 
-
-
     sei();                         // Global interrupt enable
 
     USART_print("Enter commands\n");
 
     while(1){
-        
+
         CLI_process();
+        HCSR04_src();
         adc_src();
         pwm_src();
         led_src();
+        MSG_src();
     }
 }
